e6a57c6c5817c4470b699c79da8103d7fdd91f06
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-02-26 17:26:46 +0600

    config server added

---
+++ configServer/src/test/java/site/shazan/ecommerce/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.shazan.ecommerce.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
