208fe6b30148622d9ac6a3449bf35501bff3fd48
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-02-28 01:03:27 +0600

    eureka is configured

---
+++ eurekaServer/src/test/java/site/shazan/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.shazan.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
