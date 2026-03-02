f9bbe500843f829e3d21e4c7fd29b6ee8ada7210
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-03-02 18:42:50 +0600

    zapkin added

--- order/src/main/java/site/shazan/ecommerce/order/clients/RestConfig.java
+++ order/src/main/java/site/shazan/ecommerce/order/clients/RestConfig.java
@@ -1,13 +1,26 @@
 package site.shazan.ecommerce.order.clients;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestConfig {
+    @Autowired(required = false)
+    private ObservationRegistry observationRegistry;
+
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private Propagator propagator;
 
     @Bean
     @Primary
@@ -18,6 +31,23 @@ public class RestConfig {
     @Bean
     @LoadBalanced
     public RestClient.Builder loadBalancedRestClientBuilder() {
-        return RestClient.builder();
+        RestClient.Builder builder = RestClient.builder();
+        if(observationRegistry != null){
+            builder.requestInterceptor(createTracingInterceptor());
+        }
+        return builder;
+    }
+
+    private ClientHttpRequestInterceptor createTracingInterceptor() {
+        return ((request, body, execution) -> {
+            if (tracer != null && propagator != null
+                    && tracer.currentSpan() != null) {
+                propagator.inject(tracer.currentTraceContext().context(),
+                        request.getHeaders(),
+                        (carrier, key, value) -> carrier.add(key, value));
+            }
+            return execution.execute(request, body);
+        });
+
     }
 }
