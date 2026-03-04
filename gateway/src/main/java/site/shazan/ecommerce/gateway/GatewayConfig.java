b7c66483ebfc57ab503b5ef04a02fa47adce2d2f
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-03-04 19:10:58 +0600

    fallback system added for product-service

--- gateway/src/main/java/site/shazan/ecommerce/gateway/GatewayConfig.java
+++ gateway/src/main/java/site/shazan/ecommerce/gateway/GatewayConfig.java
@@ -26,7 +26,8 @@ public class GatewayConfig {
                         .uri("http://localhost:8761"))
                 .route("product", r -> r
                         .path("/products/**")
-                        .filters(f -> f.circuitBreaker(config -> config.setName("ecomApp"))
+                        .filters(f -> f.circuitBreaker(config -> config.setName("ecomApp")
+                                        .setFallbackUri("forward:/fallback/products"))
                                        .rewritePath("/products(?<segment>/?.*)", "/api/products${segment}"))
                         .uri("lb://PRODUCT"))
                 .route("user", r -> r
